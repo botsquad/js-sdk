7f22cbaa5661357f5a5e25aff9be69a8a33a7392
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-11-14 16:07:33 +0100

    Test registering pushwoosh token

--- test/chat_bubble.test.ts
+++ test/chat_bubble.test.ts
@@ -199,7 +199,7 @@ describe('ChatBubble page views', () => {
 })
 
 describe('ChatBubble push notifications', () => {
-  it('can register a push token', async () => {
+  it('can register an Expo push token', async () => {
     const bubble = new ChatBubble(VALID_JOIN_PARAMS)
     const r = bubble.registerPushToken(PushService.EXPO, 'xx').then(
       r => expect(r).toEqual({ result: "OK"}))
@@ -207,6 +207,15 @@ describe('ChatBubble push notifications', () => {
     await bubble.connect()
 
     return r
+  })
+
+  it('can register a Pushwoosh token', async () => {
+    const bubble = new ChatBubble(VALID_JOIN_PARAMS)
+    await bubble.connect()
+    await bubble.putUserInfo({ first_name: "Pushwoosh user" })
+
+    const r = await bubble.registerPushToken(PushService.PUSHWOOSH, 'xx')
+    expect(r.result).toEqual("OK")
   })
 })
 
