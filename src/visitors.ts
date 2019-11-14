61f3c35387476282f7470f687722076e89955e0f
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-11-14 13:45:34 +0100

    Add library name + version to user agent

--- src/visitors.ts
+++ src/visitors.ts
@@ -2,6 +2,7 @@ import { Socket, Channel } from 'phoenix'
 import { SimpleEventDispatcher } from 'ste-simple-events'
 import { Config, Nudge, Internal as I } from './types'
 import { promisify } from './channel'
+import * as packageJson from '../package.json'
 
 export namespace Internal {
   type OnNudge = SimpleEventDispatcher<Nudge>
@@ -39,7 +40,7 @@ export namespace Internal {
     private joinParams(config: Config, conversationInfo: I.ConversationsJoinResponse): I.VisitorsJoinParams {
       return {
         visitor_id: conversationInfo.userId,
-        user_agent: config.userAgent,
+        user_agent: config.userAgent + ` (${packageJson.name}; ${packageJson.version})`,
         locale: config.locale,
         timezone: config.timezone,
       }
